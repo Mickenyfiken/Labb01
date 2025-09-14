14c882e1d9907d28698d3e866eab2ccfef7a7b99
Mickenyfiken <mikael.c.andersson88@gmail.com>
2025-09-14 14:22:08 +0200

    Add project files.

---
+++ Labb01/Program.cs
@@ -0,0 +1,78 @@
+﻿
+Console.Write("Skriv en text med siffror och bokstäver: ");
+string userInput = Console.ReadLine();
+int nextIndex = 0;
+int startIndex = 0;
+long sum = 0l;
+bool notNumber = false;
+bool exit = false;
+
+while (!exit)
+{
+    if (startIndex < userInput.Length - 1)
+    {
+        nextIndex = userInput.IndexOf(userInput[startIndex], startIndex + 1);
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine("Total = " + sum);
+        exit = true;
+        continue;
+    }
+
+    if (nextIndex > -1)
+    {
+
+        notNumber = StringIsDigit(userInput.Substring(startIndex, nextIndex + 1 - startIndex));
+        if (notNumber)
+        {
+            startIndex++;
+            continue;
+        }
+        else
+        {
+            sum += Int64.Parse(userInput.Substring(startIndex, nextIndex + 1 - startIndex));
+
+            PrintString(startIndex, nextIndex, userInput);
+
+
+            Console.WriteLine();
+        }
+    }
+
+    startIndex++;
+}
+
+
+static bool StringIsDigit(string partOfString)
+{
+    foreach (char letter in partOfString)
+    {
+        if (!char.IsDigit(letter))
+        {
+            return true;           
+        }
+    }
+    return false;
+}
+
+
+static void PrintString(int startIndex, int nextIndex, string userInput)
+{
+
+
+    for (int i = 0; i < userInput.Length; i++)
+    {
+        if (i >= startIndex && i <= nextIndex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(userInput[i]);
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(userInput[i]);
+        }
+    }
+}
